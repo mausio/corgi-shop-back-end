34d54876b6fa3c0d22b1d52c3ac4d1deba4f29bc
Niklas Krauth <nk@syndikat7.de>
2023-10-18 10:44:00 +0200

    Initialize SpringBoot

---
+++ src/main/java/com/pembroke/corgishopappbackend/CorgiShopAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.pembroke.corgishopappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorgiShopAppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorgiShopAppBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pembroke/corgishopappbackend/CorgiShopAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pembroke.corgishopappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorgiShopAppBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
