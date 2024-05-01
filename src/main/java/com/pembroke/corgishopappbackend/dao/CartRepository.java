276cbaf3ca2528819c71b2ad89ece840b696f96a
Mael Dossoh <mael.d@web.de>
2024-05-01 14:22:49 +0200

    added methods to cart doa

--- src/main/java/com/pembroke/corgishopappbackend/dao/CartRepository.java
+++ src/main/java/com/pembroke/corgishopappbackend/dao/CartRepository.java
@@ -1,8 +1,12 @@
 package com.pembroke.corgishopappbackend.dao;
 
-import com.pembroke.corgishopappbackend.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.pembroke.corgishopappbackend.dto.CartDTO;
+import com.pembroke.corgishopappbackend.entity.Category;
+
+import java.util.List;
 
 public interface CartRepository extends JpaRepository<Category, Integer> {
-
+    List<CartDTO> findByUserId(int userId);
+    CartDTO findByUserIdAndCartId(int userId, int cartId);
 }
